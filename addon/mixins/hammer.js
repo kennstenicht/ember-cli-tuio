2da912a9b8b2628bd777b1e59ec948d455b29421
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-09-15 09:42:51 +0200

    refactor recoginzer

--- addon/mixins/hammer.js
+++ addon/mixins/hammer.js
@@ -9,32 +9,41 @@ const {
 } = Ember;
 
 export default Mixin.create({
+  gestures: ['tap', 'press'],
+
+  recognizers: {
+    tap: {threshold: 40},
+    press: {time: 2000}
+  },
+
   setup: on('didInsertElement', function() {
-    if (!this.get('recognizers')) {
+    if (!this.get('gestures')) {
       return;
     }
 
     this.$().hammer();
 
-    this.get('recognizers').forEach((item) => {
-      if (item[1]) {
-        this.$().data('hammer').get(item[0]).set(item[1]);
+    this.get('gestures').forEach((gesture) => {
+      let options = this.get('recognizers')[gesture];
+
+      if (options) {
+        this.$().data('hammer').get(gesture).set(options);
       }
 
-      if (typeof this[item[0]] === 'function') {
-        this.$().hammer().on(item[0], bind(this, item[0]));
+      if (typeof this[gesture] === 'function') {
+        this.$().hammer().on(gesture, bind(this, gesture));
       }
     });
   }),
 
   teardown: on('willDestroyElement', function() {
-    if (!this.get('recognizers')) {
+    if (!this.get('gestures')) {
       return;
     }
 
-    this.get('recognizers').forEach((item) => {
-      if (typeof this[item[0]] === 'function') {
-        this.$().hammer().off(item[0], bind(this, item[0]));
+    this.get('gestures').forEach((gesture) => {
+      if (typeof this[gesture] === 'function') {
+        this.$().hammer().off(gesture, bind(this, gesture));
       }
     });
   })
