3dad11f2674831889eb6e049effda0ae79880517
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2015-09-23 10:19:10 +0200

    add customEvents to initializer

--- addon/instance-initializers/tuio.js
+++ addon/instance-initializers/tuio.js
@@ -1,5 +1,12 @@
 export function initialize(application) {
   application.container.lookup('service:tuio').setupClient();
+  let customEvents = application.get('customEvents') || {};
+
+  customEvents['objectadded'] = 'objectAdded';
+  customEvents['objectmove'] = 'objectMove';
+  customEvents['objectremoved'] = 'objectRemoved';
+
+  application.set('customEvents', customEvents);
 }
 
 export default {
